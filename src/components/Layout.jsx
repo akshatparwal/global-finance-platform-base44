694e32245273e6119acac253c93de72e6283c11c
Akshat Parwal <akshatparwal34@gmail.com>
2026-04-20 23:38:34 +0000

    File changes

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,15 +1,10 @@
 import { Outlet } from "react-router-dom";
-import Header from "./Header";
-import Footer from "./Footer";
-
 export default function Layout() {
   return (
     <div className="bg-background text-foreground min-h-screen">
-      <Header />
-      <main style={{ paddingTop: "env(safe-area-inset-top)" }}>
+      <main>
         <Outlet />
       </main>
-      <Footer />
     </div>
   );
 }
